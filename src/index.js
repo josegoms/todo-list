f28358b308c17104f16876e8ae216c108339477e
josegoms <josegomespinheiro26@gmail.com>
2025-05-02 23:03:15 -0300

    Add dialog to handle creating new projects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles.css";
 import { Todo } from "./todo.js";
 import { Project } from "./project.js";
 
+//Keep projects
+const projects = [];
 
 //Create todo
 const addTodo = document.querySelector(".add-todo");
@@ -17,22 +19,36 @@ addTodo.addEventListener("click", (event) => {
     //Catch the project it's inside
     const projectName = document.querySelector("#display-todos > h1").textContent;
 
-    //Create todo
-    if (projectName !== "Today") {
-        projectName.addTodo(new Todo(title, description, dueDate, priority));
-    }
+    //Create new todo and append to respective project
+    const newTodo = new Todo(title, description, dueDate, priority);
+    projectName.addTodo(newTodo);
 });
 
 //Create project
 const createProject = document.querySelector("#create-project");
-createProject.addEventListener("click", (event) => {
+createProject.addEventListener("click", () => {
+
+    //Call dialog
+    const dialog = document.querySelector("#project-form");
+    dialog.showModal();
 
     //Catch user input
-    const formData = new FormData(event.target);
-    const name = formData.get("name");
-    const description = formData.get("description");
+    const formsData = document.querySelector("#new-project");
+    formsData.addEventListener("submit", (event) => {
+        
+        //Catch user input
+        const formData = new FormData(event.target);
+        const name = formData.get("project-name");
+        const description = formData.get("project-description");
+        
+        //Create new project and append to projects
+        const newProject = new Project(name, description);
+        projects.push(newProject);
+    });
 
-    //Create new project
-    new Project(name, description);
-
+    //Cancel dialog
+    const cancel = document.querySelector(".cancel");
+    cancel.addEventListener("click", (event) => {
+        dialog.close();
+    });
 });
